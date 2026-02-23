20547ba65720846488edba2f1c7fdf7216120bee
Jhon Cardona <jhoncardonaquintero@gmail.com>
2026-02-23 15:56:50 -0500

    Personalización del componente App

--- agenda-adso/src/App.jsx
+++ agenda-adso/src/App.jsx
@@ -1,4 +1,21 @@
-import './App.css';
-export default function App()
-{const fecha = new Date().toLocaleString();
-return ( <main> <h1>Hola SENA</h1> </main>)}
+import './App.css'
+
+export default function App() {
+  const fecha = new Date().toLocaleString();
+
+  return (
+    <main>
+      <h1>Hola, Soy Jhon Fredy Cardona Quintero - Aprendiz ADSO</h1>
+
+      <p>
+        En este curso de React espero aprender a crear aplicaciones web modernas,
+        dinámicas y reutilizables utilizando componentes, props y hooks.
+        Mi objetivo es fortalecer mis habilidades como desarrollador frontend,
+        entender mejor el manejo del estado y desarrollar proyectos más
+        organizados y profesionales.
+      </p>
+
+      <small>{fecha}</small>
+    </main>
+  );
+}
