527e2dc22ea1cca2fcb06a5e262b02115c969556
Jhon Cardona <jhoncardonaquintero@gmail.com>
2026-03-18 13:40:15 -0500

    Clase_12_Agenda_ADSO_Conectada_a_render

--- src/config.js
+++ src/config.js
@@ -5,4 +5,4 @@ export const APP_INFO = {
   ficha: "3229209",
   titulo: "Agenda ADSO v9",
   subtitulo: "Gestión de contactos conectada a una API local con JSON Server, con validaciones y mejor experiencia de usuario.",
-};|
+}
