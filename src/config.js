3c8aff04bd5126acb71b950a47c6e91e83d12d26
Jhon Cardona <jhoncardonaquintero@gmail.com>
2026-03-18 13:37:53 -0500

    Clase_12_Agenda_ADSO_Conectada_a_render

--- src/config.js
+++ src/config.js
@@ -1,8 +1,8 @@
 // Archivo: src/config.js
-export const API_BASE_URL = "http://localhost:3000/contactos";
+export const API_BASE_URL = "https://agenda-adso-api-d71h.onrender.com/contactos"; // ← elige el que uses
 
 export const APP_INFO = {
   ficha: "3229209",
   titulo: "Agenda ADSO v9",
   subtitulo: "Gestión de contactos conectada a una API local con JSON Server, con validaciones y mejor experiencia de usuario.",
-};
+};|
